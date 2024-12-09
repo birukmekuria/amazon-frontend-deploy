22a5c15d6c0798dccc1145f540a82b3ec617713c
Biruk Mekuria <brkassefa@gmail.com>
2024-12-09 22:17:46 +0300

    deploy

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/clone-a047b/us-central1/api",
 
   //Deployed version of amazon server on render.com
-  baseURL: "https://amazon-api-deploy-1-k3pp.onrender.com/",
+  baseURL: "https://amazon-api-deploy-9yvw.onrender.com",
 });
 
 export { axiosInstance };
